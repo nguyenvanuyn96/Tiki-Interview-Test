45dfef89fbe79c0f322c61b011857bdf145faddb
Uy Nguyen <nvuy@fossil.com>
2018-05-23 15:12:48 +0700

    Init project

---
+++ Tiki-Interview-Test/Tiki-Interview-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tiki-Interview-Test
+//
+//  Created by Nguyen Van Uy on 5/23/18.
+//  Copyright © 2018 Uy Nguyen Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
